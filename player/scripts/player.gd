2f8c70ad1660a4a07977a9412c99c9714ede831c
dario <welcomedario9@gmail.com>
2026-04-25 16:08:45 -0600

    cree los scripts para cada estado

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -5,12 +5,69 @@ var states : Array [ PlayerState ]
 var current_state : PlayerState :
 	get : return states.front()
 var previous_state : PlayerState :
-	get : return states[1]
+	get : return states[ 1 ]
 #endregion
 
-func _process(_delta: float) -> void:
+#region /// Variables estándar
+var direction : Vector2 = Vector2.ZERO
+var gravity : float = 980
+#endregion
+
+func _ready() -> void:
+	initialize_states()
 	pass
 
-# Movimiento Básico del jugador
-func _physics_process(_delta: float) -> void:
+func _unhandled_input(event: InputEvent) -> void:
+	change_state(current_state.handle_input(event))
+	pass
+
+func _process( _delta: float ) -> void:
+	change_state(current_state.process(_delta))
+	pass
+
+func _physics_process( _delta: float ) -> void:
+	velocity.y += gravity * _delta
+	move_and_slide()
+	change_state(current_state.physics_process(_delta))
+	pass
+
+func initialize_states() -> void:
+	states = []
+	# Enlazar todos los estados y los despliega en consola
+	for c in $States.get_children():
+		if c is PlayerState:
+			states.append( c )
+			c.player = self
+		pass
+	
+	# Inicializar todos los estados
+	for state in states:
+		state.init()
+	
+	# Previene fallos en caso de no haber estados hijos
+	if states.size() == 0:
+		return
+	
+	# Establecer nuestro primer estado
+	change_state( current_state )
+	current_state.enter()
+	pass
+
+func change_state( new_state : PlayerState ) -> void:
+	if new_state == null:
+		return
+	elif new_state == current_state:
+		return
+		
+	if current_state:
+		current_state.exit()
+	
+	states.push_front( new_state )
+	current_state.enter()
+	states.resize( 3 )
+	pass
+
+func update_direction() -> void:
+	#var prev_direction : Vector2 = direction
+	direction = Input.get_vector("left", "right", "up", "down")
 	pass
---
+++ player/states/idle.gd
@@ -0,0 +1,25 @@
+class_name PlayerStateIdle extends PlayerState
+
+# Lo que pasa cuado este estado es inicializado
+func init() -> void:
+	pass
+
+# Lo que pasa cada vez que entremos al estado
+func enter() -> void:
+	pass
+
+# Lo que pasas cuando salimos del estado
+func exit() -> void:
+	pass
+
+# Manejador de entradas
+func handle_input(_event: InputEvent) -> PlayerState:
+	return next_state
+
+# Lo que pasa en cada ciclo de procesos del estado
+func process(_delta: float) -> PlayerState:
+	return next_state
+
+# Lo que pasa con cada ciclo de procesos fisicos
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
